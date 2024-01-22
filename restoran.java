5ac25fcdf81499dc7df2cf4f5c256bd0bdca373f
salvestacia <144323961+salvestacia@users.noreply.github.com>
2024-01-22 20:56:31 +0700

    java code to create a cashier application system in a restaurant

--- restoran.java
+++ restoran.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
 public class restoran {
     static Scanner input = new Scanner(System.in);
     static List<Integer> listSubtotalHargaMakanan = new ArrayList<>();
